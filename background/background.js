0a211d87eb9bbc86423566d73fdf8a8d92b95ebe
Gustaf Liljegren <gustafl@bahnhof.se>
2017-03-19 15:30:47 +0100

    Work on issue #10 (incomplete).

--- background/background.js
+++ background/background.js
@@ -1,55 +1,55 @@
 var URL_PATTERN = '://www.youtube.com/watch?v=';
 
+/**
+ * Fires when the frame's history was updated to a new URL. We use this instead
+ * of onUpdated, due to some particularities of YouTube.
+ */
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
     if (details.frameId === 0) {
         chrome.tabs.get(details.tabId, function (tab) {
-            if (tab.url === details.url && tab.url.indexOf(URL_PATTERN) > -1) {
-                console.log('onHistoryStateUpdated: ' + tab.url);
-                chrome.pageAction.show(tab.id);
+            if (tab.url === details.url) {
+                doStuff(tab);
             }
         });
     }
 });
 
+/**
+ * Fires when the active tab in a window changes.
+ */
 chrome.tabs.onActivated.addListener(function (activeInfo) {
     chrome.tabs.get(activeInfo.tabId, function (tab) {
-        if (tab.url.indexOf(URL_PATTERN) > -1) {
-            console.log('onActivated: ' + tab.url);
-            chrome.pageAction.show(tab.id);
-        }
+        doStuff(tab);
     });
 });
 
-/*chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (tab.status == 'complete') {
-        console.log(tab);
-        chrome.tabs.get(tabId, function (tab) {
-            if (tab.url.indexOf(URL_PATTERN) > -1) {
-                console.log('onUpdated: ' + tab.url);
-                chrome.pageAction.show(tab.id);
-            }
+/**
+ * Enables the toolbar button and requests an array of distinct users from the
+ * content script.
+ * @param {chrome.tabs.Tab} tab
+ */
+function doStuff(tab) {
+    if (tab.url.indexOf(URL_PATTERN) > -1) {
+        chrome.pageAction.show(tab.id);
+        var message = { name: 'getUsers' };
+        chrome.tabs.sendMessage(tab.id, { message: message }, function (response) {
+            console.log(response);
         });
     }
-});*/
+}
 
-/*chrome.runtime.onMessage.addListener(function (request, sender, response) {
-    if (request.action == 'show') {
-        console.info('runtime.onMessage');
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            if (tabs.length > 0) {
-                
-            } 
-        });
-    }
-});*/
-
-/*chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete') {
-        console.info('tabs.onUpdated');
-        console.log(tabId);
-        console.log(changeInfo);
-        console.log(tab);
-        
+/**
+ * Listens for various messages. Each message object (request.message) has a
+ * name and data property.
+ */
+chrome.runtime.onMessage.addListener(function (request, sender, response) {
+    switch (request.message.name) {
+        case 'getUsers':
+            var users = request.message.data;
+            console.log(users);
+            break;
+        default:
+            console.warn('Unknown message: ' + request.message.name);
+            break;
     }
 });
-*/
--- content_scripts/youtube.js
+++ content_scripts/youtube.js
@@ -1,7 +1,5 @@
 'use strict';
 
-chrome.runtime.sendMessage({ action: 'show' });
-
 /**
  * Available functions in chrome.*
  * ===============================
@@ -18,33 +16,38 @@ chrome.runtime.sendMessage({ action: 'show' });
  */
 
 /**
- * If your script definitely needs to run after window.onload, you can check
- * if onload has already fired by using the document.readyState property.
- */
-
-/**
  * Returns a list of distinct users (commenters) from the page. 
  */
 function getUsers() {
     var users = [];
-    var comments = document.querySelector('div.comment-renderer');
-    comments.forEach(function (currentValue, currentIndex, listObj, argument) {
-        var element = currentValue.querySelector('a');
+    var nodeList = document.querySelector('div.comment-renderer');
+    for(var node of nodeList.values()) { 
+        var element = node.querySelector('a');
         var userId = element.getAttribute('data-ytid');
         if (users.indexOf(userId) === -1) {
             users.push(userId);
         }
-    });
+    }
     return users;
 }
 
 /**
- * Receive array of blacklisted users
+ * Listens for various messages. Each message object (request.message) has a
+ * name and data property.
  */
-chrome.runtime.onMessage.addListener(function (request, sender, response) {
-    if (request.blacklistedUsers) {
-        console.info('We got some blacklisted users!');
-        hideComments(request.blacklistedUsers);
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    console.log(request);
+    switch (request.message.name) {
+        case 'getUsers':
+            console.log(request.message.name);
+            //var users = getUsers();
+            var users = 'Gustaf';
+            var message = { name: 'getUsers', data: users };
+            sendResponse({ message: message });
+            break;
+        default:
+            console.warn('Unknown message: ' + request.message.name);
+            break;
     }
 });
 
