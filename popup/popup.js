4aac7c4d870f39603aac8c4dfc7fcfd5a90f9134
Gustaf Liljegren <gustafl@bahnhof.se>
2017-04-02 17:20:39 +0200

    More work on popup layout

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,27 @@
 'use strict';
 
 console.log('Hello from popup!');
+
+var button = document.querySelector('button.disable');
+
+var blacklisted = document.querySelector('.blacklisted');
+var blacklistedDetails = document.querySelector('.blacklisted-details');
+blacklisted.addEventListener('click', function () {
+    if (blacklistedDetails.classList.contains('hidden')) {
+        blacklistedDetails.classList.remove('hidden');
+    } else {
+        blacklistedDetails.classList.add('hidden');
+    }
+});
+
+
+var whitelisted = document.querySelector('.whitelisted');
+var whitelistedDetails = document.querySelector('.whitelisted-details');
+whitelisted.addEventListener('click', function () {
+    if (whitelistedDetails.classList.contains('hidden')) {
+        whitelistedDetails.classList.remove('hidden');
+    } else {
+        whitelistedDetails.classList.add('hidden');
+    }
+});
+
